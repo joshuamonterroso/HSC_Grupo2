6d45dc8fb88c719a98e66200adf0729bcfa803f2
joshua monterroso <amonterrosoe4@miumg.edu.gt>
2021-09-25 21:01:44 -0600

    Version funcional de consultas inteligentes

---
+++ ConsultasInteligentes/consultasinteligentes/CapaControlador/clscontrolador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaControlador
+{
+    public class clscontrolador
+    {
+        clsSentencias sn = new clsSentencias();
+        //Llenar una tabla capa controlador
+        public DataTable llenartb1(string tabla)
+        {
+            OdbcDataAdapter dt = sn.llenartb1(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+        public OdbcDataReader llenarcbo()
+        {
+            string sql = "show full tables from prototipo_area_compras;";
+            return sn.llenarcbotabla(sql);
+        }
+
+        public OdbcDataReader llenarcbo2(string tabla)
+        {
+            string sql = "show columns from "+ tabla +";";
+            return sn.llenarcbotabla(sql);
+        }
+
+        public OdbcDataReader llenarcboq(string tabla)
+        {
+            string sql = "select nombre, consulta from " + tabla + ";";
+            return sn.llenarcbotabla(sql);
+        }
+        public void ingresarconsulta(string nombre, string consulta)
+        {
+            string sql = "insert into registro_consultas (nombre,consulta) values ( '"+nombre+"', '"+consulta+  "') ;";
+            Console.WriteLine(sql);
+            sn.insertarconsulta(sql);
+        }
+        
+    }
+
+}
---
+++ ConsultasInteligentes/consultasinteligentes/CapaVista/ConsultasInteligentes.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+using System.Data.Odbc;
+
+
+namespace CapaVista
+{
+    public partial class ConsultasInteligentes : Form
+    {
+        string campo = "";
+        string csimple = "";
+        string where = "";
+        string and = "";
+        string group = "";
+        public ConsultasInteligentes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelarCamposSeleccionados_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            habilitaciones();
+        }
+        clscontrolador cn = new clscontrolador();
+
+        private void ConsultasInteligentes_Load(object sender, EventArgs e)
+        {
+            llenarcombo();
+            llenarcboquery();           
+        }
+
+        string consulta = "";
+        public void actualizardatagrid()
+        {
+            DataTable dt = cn.llenartb1(consulta);
+            dgvCONSULTAS.DataSource = dt;
+        }
+        public void llenarcombo()
+        {
+            cboTabla.Items.Clear();
+            OdbcDataReader datareader = cn.llenarcbo();
+            while (datareader.Read())
+            {
+                cboTabla.Items.Add(datareader[0].ToString());
+            }
+        }
+        public void llenarcombo2()
+        {
+            cboCampos.Items.Clear();
+            cboCampoLogica.Items.Clear();
+            cboCampoComparacion.Items.Clear();
+            cboCampoAgruparOrdenar.Items.Clear();
+            OdbcDataReader datareader = cn.llenarcbo2(valortabla.Text);
+            while (datareader.Read())
+            {
+                cboCampos.Items.Add(datareader[0].ToString());
+                cboCampoLogica.Items.Add(datareader[0].ToString());
+                cboCampoComparacion.Items.Add(datareader[0].ToString());
+                cboCampoAgruparOrdenar.Items.Add(datareader[0].ToString());
+            }
+        }
+
+        string query = "registro_consultas";
+        public void llenarcboquery()
+        {
+            cboQuery.Items.Clear();
+            cbocopiaquery.Items.Clear();
+            OdbcDataReader datareader = cn.llenarcboq(query);
+            while (datareader.Read())
+            {
+                cboQuery.Items.Add(datareader[0].ToString());
+                cbocopiaquery.Items.Add(datareader[1].ToString());
+            }
+        }
+        private void cboTabla_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           valortabla.Text = cboTabla.SelectedItem.ToString();
+            llenarcombo2();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (chkSelectTodos.Checked == false)
+            {            
+                if (txtcamposelectos.Text.Equals(""))
+                {
+                    if (txtAlias.Text.Equals(""))
+                    {
+                        txtcamposelectos.Text = "";
+                        campo = campo + cboCampos.SelectedItem.ToString() + " ";
+                        txtcamposelectos.Text = campo;
+                    } else
+                    {
+                        txtcamposelectos.Text = "";
+                        campo = campo + cboCampos.SelectedItem.ToString() + " as "+ txtAlias.Text +" ";
+                        txtcamposelectos.Text = campo;
+                    }
+                }
+                else{
+                    if (txtAlias.Text.Equals(""))
+                    {
+                        txtcamposelectos.Text = "";
+                        campo =  campo +", " + cboCampos.SelectedItem.ToString() + " ";
+                        txtcamposelectos.Text = campo;
+                    }
+                    else
+                    {
+                        txtcamposelectos.Text = "";
+                        campo =  campo + ", " + cboCampos.SelectedItem.ToString() + " as " + txtAlias.Text + " ";
+                        txtcamposelectos.Text = campo;
+                    }
+                }
+            } else
+            {
+                txtcamposelectos.Text = "";
+                campo = "";
+                campo = " * ";
+                txtcamposelectos.Text = "Todos los campos seleccionados";
+                Console.WriteLine(campo);
+            }
+        }
+
+        private void chkSelectTodos_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSelectTodos.Checked == true)
+            {
+                txtAlias.Text = "";
+                txtAlias.Enabled = false;
+                cboCampos.Text = "";
+                cboCampos.Enabled = false;
+                txtcamposelectos.Text = "";
+            } else
+            {
+                txtAlias.Text = "";
+                txtAlias.Enabled = true;
+                cboCampos.Text = "";
+                cboCampos.Enabled = true;
+            }
+        }
+        private void btprueba_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkcondiciones_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkcondiciones.Checked == true)
+            {
+                gpbConsultaCompleja.Enabled = true;
+                gpbAgruparUOrdenar.Enabled  = true;
+            } else
+            {
+                gpbConsultaCompleja.Enabled =  false;
+                gpbAgruparUOrdenar.Enabled  =  false;
+            }
+        }
+
+        private void btnAgregarCamposSeleccionados_Click(object sender, EventArgs e)
+        {
+            csimple = "SELECT " + campo + "FROM " + valortabla.Text + " " ;
+            MessageBox.Show("La cadena generada es: " + csimple);
+            Console.WriteLine(csimple);
+            txtCadenaGenerada.Text = csimple;
+            campo = "";
+            txtAlias.Text = "";
+            cboCampos.Text = "";         
+            txtcamposelectos.Text = "";
+            cboTabla.Text = "";
+            chkSelectTodos.Checked = false;
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            csimple = csimple + " ;";
+            MessageBox.Show("La consulta Generado fue: " + csimple);
+            cn.ingresarconsulta(txtNombreConsulta.Text, txtCadenaGenerada.Text);
+            llenarcboquery();
+            limpiar();
+            habilitaciones();
+        }
+
+        public void limpiar()
+        {
+            txtCadenaGenerada.Text = "";
+            txtAlias.Text = "";
+            txtNombreConsulta.Text = "";
+            valortabla.Text = "";
+            txtValor.Text = "";
+            txtValorComparacion.Text = "";
+            cboAgruparOrdenar.Text = "";
+            cboCampos.Text = "";
+            cboTabla.Text = "";
+            cboOperadorLogico.Text = "";
+            cboCampoLogica.Text = "";
+            cboTipoComparador.Text = "";
+            cboCampoComparacion.Text = "";
+            cboCampoAgruparOrdenar.Text = "";
+            campo = "";
+            csimple = "";
+        }
+
+        public void habilitaciones()
+        {
+            gpbConsultaCompleja.Enabled = false;
+            gpbAgruparUOrdenar.Enabled  = false;
+            chkcondiciones.Checked      = false;
+            chkSelectTodos.Checked      = false;
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            habilitaciones();
+        }
+
+        private void btnBuscarCONSULTAS_Click(object sender, EventArgs e)
+        {
+            actualizardatagrid();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboQuery_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbocopiaquery.SelectedIndex = cboQuery.SelectedIndex;
+            txtCadenaGeneradaCONSULTAS.Text = cbocopiaquery.SelectedItem.ToString();
+            consulta = txtCadenaGeneradaCONSULTAS.Text;
+        }
+
+        private void cbocopiaquery_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cboCampos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
