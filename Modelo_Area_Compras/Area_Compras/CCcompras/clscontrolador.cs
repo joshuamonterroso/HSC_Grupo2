d302803bee4e64dab0ef1529e1569b29306b8c18
joshua monterroso <amonterrosoe4@miumg.edu.gt>
2021-10-09 17:41:46 -0600

    Modelo area compras V1

---
+++ Modelo_Area_Compras/Area_Compras/CCcompras/clscontrolador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMcompras;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CCcompras
+{
+    public class clscontrolador
+    {
+        //Instanciamos la clase sentencias
+        //Adolfo Monterroso 0901-18-50 09/10/2021
+        clssentencias sn = new clssentencias();
+    }
+}
---
+++ Modelo_Area_Compras/Area_Compras/CVcompras/Area_Compras.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CCcompras;
+using System.Data.Odbc;
+
+namespace CVcompras
+{
+    public partial class Area_Compras : Form
+    {
+        clscontrolador cn = new clscontrolador();
+        public Area_Compras()
+        {
+            InitializeComponent();
+        }
+
+     //Direccionamiento de formularios desde mdi
+     //Adolfo Monterroso 0901-18-50 09/10/2021
+
+        private void Area_Compras_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
